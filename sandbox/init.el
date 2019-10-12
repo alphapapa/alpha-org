31ec5cc601850eccbd479316056d61429d27f2dd
Adam Porter <adam@alphapapa.net>
2019-10-12 00:37:50 -0500

    Add: sandbox/init.el
    Helpful for development and testing.

---
+++ sandbox/init.el
@@ -0,0 +1,26 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(require 'alpha-org nil t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-sticky-header org-ql peg ov org-super-agenda ts ht f dash-functional org-make-toc s dash org-bullets use-package bind-key quelpa org-sidebar org-plus-contrib general alpha-org))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
